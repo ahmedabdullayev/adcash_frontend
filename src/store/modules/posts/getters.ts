b32febf1f324faff2e7f2c0a26e03a67448786a1
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2022-03-04 20:17:00 +0200

    update filter

--- src/store/modules/posts/getters.ts
+++ src/store/modules/posts/getters.ts
@@ -4,7 +4,7 @@ import _ from 'lodash';
 
 export default {
     filteredPost: (state: PostState) => (word: string) : Posts[] =>{
-        return _.values(_.pickBy(state.post, article => article.content.toLocaleLowerCase().includes(word.toLowerCase())));
+        return _.values(_.pickBy(state.post, article => article.content?.toLocaleLowerCase().includes(word.toLowerCase())));
     },
     post: (state: PostState) : Posts[] => {
         return state.post;
