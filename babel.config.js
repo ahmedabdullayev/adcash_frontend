b0c567b7c2cae48e39f2d8ebbad25a14c94d6b05
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2022-03-08 14:45:52 +0200

    tests fixed

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
-    "@babel/preset-env"
-  ]
+    "@babel/preset-env",
+  ],
+  env: {
+    test: {
+      plugins: ["transform-require-context"],
+    },
+  },
 }
--- tests/unit/notFound.spec.ts
+++ tests/unit/notFound.spec.ts
@@ -2,18 +2,17 @@ import { shallowMount } from '@vue/test-utils'
 import notfound from '@/components/notfound.vue'
 import i18n from "@/i18n";
 
-describe('HelloWorld.vue', () => {
+describe('notfound.vue', () => {
     it('renders props.msg when passed', () => {
         const i18nn = i18n
 
-        const msg = 'new message'
+        const msg = 'some message'
         const wrapper = shallowMount(notfound, {
             props: { msg }
             ,global: {
                 plugins: [i18nn]
             }
         })
-        console.log(wrapper.text())
         expect(wrapper.text()).toMatch(msg)
     })
 })
